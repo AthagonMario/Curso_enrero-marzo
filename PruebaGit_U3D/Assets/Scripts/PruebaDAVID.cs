4e2979a72c4f3ea322c3f2bc58881c9032e95e3b
Disoluto <127203514+Disoluto@users.noreply.github.com>
2023-03-08 13:31:52 +0100

    Añadido Script

---
+++ PruebaGit_U3D/Assets/Scripts/PruebaDAVID.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaDAVID : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //d
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
